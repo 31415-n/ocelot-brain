68d720aa40d74f6d60f18dcff562dd589d792839
Dmitry Zhidenkov <dmitry.zhidenkov@gmail.com>
2018-09-17 16:50:44 +0200

    reimplementation: memory

---
+++ src/main/scala/totoro/ocelot/brain/entity/Memory.scala
@@ -0,0 +1,30 @@
+package totoro.ocelot.brain.entity
+
+import totoro.ocelot.brain.entity.traits.{CallBudget, Tiered}
+import totoro.ocelot.brain.environment.traits.DeviceInfo.{DeviceAttribute, DeviceClass}
+import totoro.ocelot.brain.environment.traits.{DeviceInfo, Environment}
+import totoro.ocelot.brain.network.{Network, Node, Visibility}
+import totoro.ocelot.brain.util.Tier
+import totoro.ocelot.brain.{Constants, Settings}
+
+class Memory(override var tier: Int) extends Environment with DeviceInfo with Tiered with traits.Memory with CallBudget {
+  override val node: Node = Network.newNode(this, Visibility.Neighbors).
+    create()
+
+  private final lazy val deviceInfo = Map(
+    DeviceAttribute.Class -> DeviceClass.Memory,
+    DeviceAttribute.Description -> "Memory bank",
+    DeviceAttribute.Vendor -> Constants.DeviceInfo.DefaultVendor,
+    DeviceAttribute.Product -> ("MRAM 1x" + tier.toString),
+    DeviceAttribute.Clock -> (Settings.get.callBudgets(tier) * 1000).toInt.toString
+  )
+
+  override def getDeviceInfo: Map[String, String] = deviceInfo
+
+  override def amount: Double = {
+    val sizes = Settings.get.ramSizes
+    sizes(tier max 0 min (sizes.length - 1))
+  }
+
+  override def callBudget: Double = Settings.get.callBudgets((tier / 2) max Tier.One min Tier.Three)
+}
--- src/main/scala/totoro/ocelot/demo/Demo.scala
+++ src/main/scala/totoro/ocelot/demo/Demo.scala
@@ -1,6 +1,7 @@
 package totoro.ocelot.demo
 
 import totoro.ocelot.brain.Ocelot
+import totoro.ocelot.brain.entity.Memory
 import totoro.ocelot.brain.entity.{CPU, Cable, Case}
 import totoro.ocelot.brain.network.Network
 import totoro.ocelot.brain.util.Tier
@@ -17,6 +18,9 @@ object Demo extends App {
 
   val cpu = new CPU(Tier.Three)
   computer.add(cpu)
+
+  val memory = new Memory(Tier.Six)
+  computer.add(memory)
 
   computer.turnOn()
 }
