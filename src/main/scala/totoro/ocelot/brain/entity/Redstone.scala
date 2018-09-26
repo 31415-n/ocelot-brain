6a81ac396cf07045153645a7834e9910e26430b4
Laine <laineprikol@gmail.com>
2018-09-26 18:18:42 +0300

    add incomplete redstone card support

---
+++ src/main/scala/totoro/ocelot/brain/entity/Redstone.scala
@@ -0,0 +1,51 @@
+package totoro.ocelot.brain.entity
+
+import totoro.ocelot.brain.Constants
+import totoro.ocelot.brain.entity.traits.DeviceInfo
+import totoro.ocelot.brain.entity.traits.DeviceInfo.{DeviceAttribute, DeviceClass}
+import totoro.ocelot.brain.machine.{Arguments, Callback, Context}
+import totoro.ocelot.brain.network.{Network, Node, Visibility}
+
+class Redstone() extends Environment with DeviceInfo {
+
+  override val node: Node = Network.newNode(this, Visibility.Neighbors).
+    withComponent("redstone", Visibility.Neighbors).
+    create()
+
+  private final lazy val deviceInfo = Map(
+    DeviceAttribute.Class -> DeviceClass.Communication,
+    DeviceAttribute.Description -> "Combined redstone controller",
+    DeviceAttribute.Vendor -> Constants.DeviceInfo.DefaultVendor,
+    DeviceAttribute.Product -> "Rx900-M",
+    DeviceAttribute.Capacity -> "65536",
+    DeviceAttribute.Width -> "16"
+  )
+
+  override def getDeviceInfo: Map[String, String] = deviceInfo
+
+  val redState = new Array[Int](6) // Out
+
+  //
+  val redInState = new Array[Int](6) // In
+
+  @Callback(doc = """function(side:number, value:number):number -- Set the redstone output on the specified side.""")
+  def setOutput(context: Context, args: Arguments): Array[AnyRef] = this.synchronized {
+    val power = args.checkInteger(1)
+    val side = args.checkInteger(0)
+    redState(side) = power
+    result()
+  }
+
+  @Callback(doc = """function(side:number):number -- Get the redstone output on the specified side.""")
+  def getOutput(context: Context, args: Arguments): Array[AnyRef] = this.synchronized {
+    val side = args.checkInteger(0)
+    result(redState(side))
+  }
+
+  @Callback(doc = """function(side:number):number -- Get the redstone input on the specified side.""")
+  def getInput(context: Context, args: Arguments): Array[AnyRef] = this.synchronized {
+    val side = args.checkInteger(0)
+    result(redInState(side))
+  }
+
+}
