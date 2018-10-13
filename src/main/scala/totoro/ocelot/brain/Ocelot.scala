0b81e82355473e0f780e6b220d7f06a373df67ca
Dmitry <dmitry.zhidenkov@gmail.com>
2018-10-13 22:12:56 +0200

    version 0.2.2

--- src/main/scala/totoro/ocelot/brain/Ocelot.scala
+++ src/main/scala/totoro/ocelot/brain/Ocelot.scala
@@ -12,7 +12,7 @@ import totoro.ocelot.brain.machine.{MachineAPI, Registry}
 object Ocelot {
   final val Name = "Ocelot"
   // do not forget to change the version in `build.sbt`
-  final val Version = "0.2.1"
+  final val Version = "0.2.2"
 
   def log: Logger = logger.getOrElse(LogManager.getLogger(Name))
   var logger: Option[Logger] = None
