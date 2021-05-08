ac7c7aa8eed4623310d08fbcb7dc95b3e12f125d
UnicornFreedom <dmitry.zhidenkov@gmail.com>
2021-05-08 03:47:29 +0200

    fix: do not register copies of the same wireless endpoint

--- src/main/scala/totoro/ocelot/brain/network/WirelessNetwork.scala
+++ src/main/scala/totoro/ocelot/brain/network/WirelessNetwork.scala
@@ -1,14 +1,13 @@
 package totoro.ocelot.brain.network
 
 import scala.collection.mutable
-import scala.collection.mutable.ArrayBuffer
 
 object WirelessNetwork {
-  val _endpoints: ArrayBuffer[WirelessEndpoint] = mutable.ArrayBuffer.empty[WirelessEndpoint]
+  val _endpoints: mutable.HashSet[WirelessEndpoint] = mutable.HashSet.empty[WirelessEndpoint]
 
   def add(endpoint: WirelessEndpoint): Unit = _endpoints += endpoint
 
   def remove(endpoint: WirelessEndpoint): Unit = _endpoints -= endpoint
 
-  def endpoints: ArrayBuffer[WirelessEndpoint] = _endpoints
+  def endpoints: mutable.HashSet[WirelessEndpoint] = _endpoints
 }
