9b336704ba0ce200fc55e2e8d06e330f7acd1df4
UnicornFreedom <dmitry.zhidenkov@gmail.com>
2021-09-04 18:47:22 +0200

    upgrade: fix cpu to use default lua arch (5.3)

--- src/main/scala/totoro/ocelot/brain/Ocelot.scala
+++ src/main/scala/totoro/ocelot/brain/Ocelot.scala
@@ -32,10 +32,21 @@ object Ocelot {
 
   private def init(): Unit = {
     log.info("Registering available machine architectures...")
+
+    // Weird JNLua bug identified
+    // When loading JNLua (for either 5.2 or 5.3 lua state) there is a static section that the library loads
+    // being static, it loads once regardless of which lua state is loaded first
+    // static { REGISTRYINDEX = lua_registryindex(); }
+    // The problem is that lua_registryindex was removed in 5.3
+    // Thus, if we load JNLua from a lua5.3 state first, this static section fails
+    // We must load 5.2 first, AND we know 5.3 will likely fail to load if 5.2 failed
+    val include52: Boolean = LuaStateFactory.include52
+
+    // now that JNLua has been initialized from a lua52 state, we are safe to check 5.3
     if (LuaStateFactory.include53) {
       MachineAPI.add(classOf[NativeLua53Architecture], "Lua 5.3")
     }
-    if (LuaStateFactory.include52) {
+    if (include52) {
       MachineAPI.add(classOf[NativeLua52Architecture], "Lua 5.2")
     }
     if (MachineAPI.architectures.isEmpty) {
