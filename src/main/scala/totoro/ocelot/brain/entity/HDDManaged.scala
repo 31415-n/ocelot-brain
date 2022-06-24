d2724cadbf646f324edec9855eb62ab5ba1acdf9
UnicornFreedom <dmitry.zhidenkov@gmail.com>
2022-06-24 18:07:04 +0200

    fix persistence of HDD managed

--- src/main/scala/totoro/ocelot/brain/entity/HDDManaged.scala
+++ src/main/scala/totoro/ocelot/brain/entity/HDDManaged.scala
@@ -4,11 +4,14 @@ import totoro.ocelot.brain.Settings
 import totoro.ocelot.brain.entity.traits.{DiskManaged, Entity, Tiered}
 import totoro.ocelot.brain.event.FileSystemActivityType.{ActivityType, HDD}
 
-class HDDManaged(override var tier: Int, address: String = null) extends Entity with DiskManaged with Tiered {
+class HDDManaged(override var tier: Int) extends Entity with DiskManaged with Tiered {
   def capacity: Int = Settings.get.hddSizes(tier) * 1024
   def speed: Int = tier + 2
 
-  _address = Option(address)
+  def this(tier: Int, address: String) {
+    this(tier)
+    _address = Option(address)
+  }
 
   override val activityType: Option[ActivityType] = Some(HDD)
 }
