d03634ecdea5ae42fb9ccbce4474c541e04538cb
Fingercomp <fingercomp@fomalhaut.me>
2025-02-01 02:26:55 +0300

    Persist the tier of component buses
    Fixes #41.

--- src/main/scala/totoro/ocelot/brain/entity/ComponentBus.scala
+++ src/main/scala/totoro/ocelot/brain/entity/ComponentBus.scala
@@ -2,13 +2,13 @@ package totoro.ocelot.brain.entity
 
 import totoro.ocelot.brain.Settings
 import totoro.ocelot.brain.entity.machine.Architecture
-import totoro.ocelot.brain.entity.traits.{Entity, Environment, Processor, Tiered}
+import totoro.ocelot.brain.entity.traits.{Entity, Environment, Processor, TieredPersistable}
 import totoro.ocelot.brain.network.{Network, Node, Visibility}
 import totoro.ocelot.brain.util.Tier.Tier
 
 import java.util.UUID
 
-class ComponentBus(var tier: Tier) extends Entity with Environment with Tiered with Processor {
+class ComponentBus(var tier: Tier) extends Entity with Environment with TieredPersistable with Processor {
   // FIXME: RETARD NODE ALERT, IT CAN'T HAVE ADDRESS, BUT NEED TO BE AVAILABLE AS SLOT ITEM, FIX THIS LATER
   override val node: Node =
     Network
--- src/main/scala/totoro/ocelot/brain/entity/traits/Tiered.scala
+++ src/main/scala/totoro/ocelot/brain/entity/traits/Tiered.scala
@@ -4,6 +4,8 @@ import totoro.ocelot.brain.util.Tier.Tier
 
 /**
   * Implemented by entities that have a meaningful tier.
+  *
+  * @see TieredPersistable
   */
 trait Tiered {
   def tier: Tier
